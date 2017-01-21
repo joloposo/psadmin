01012dca1c9ef3ed6cfbc8e5058e00467d04c8de
Michal Vavrek <vavrek@duxsoft.sk>
2017-01-21 14:13:17 +0100

    React Forms - Controlled Components

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -1,8 +1,11 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = require('react-router').Link;
 var AuthorApi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
+
 
 var AuthorPage = React.createClass({
     getInitialState: function () {
@@ -22,6 +25,7 @@ var AuthorPage = React.createClass({
         return (
             <div>
                 <h1>Authors</h1>
+                <Link to="addAuthor" className="btn btn-default">Add Author</Link>
                 <AuthorList authors={this.state.authors}/>
             </div>
         );
